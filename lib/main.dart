340efaaee40c28fe6181350d07948aec6adf7fa4
Ipcarrara <ipcarrara@gmail.com>
2023-11-08 13:11:20 -0300

    Impedir a rotação da tela do aplicativo
    Este commit inclui alterações no código para desabilitar a rotação da tela do aplicativo. Isso foi feito para melhorar a experiência do usuário ao manter a orientação da tela fixa em modo retrato. Esta alteração resolve um problema de usabilidade.

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,33 @@ import 'package:flutter/material.dart';
 
 import 'package:bebometro/bebometro.dart';
 
+import 'package:flutter/services.dart';
+
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 255, 182, 24),
-                Color.fromARGB(255, 240, 146, 23),
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then(
+    (fn) {
+      runApp(
+        MaterialApp(
+          home: Scaffold(
+            body: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(255, 255, 182, 24),
+                    Color.fromARGB(255, 240, 146, 23),
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+              child: const Bebometro(),
             ),
           ),
-          child: const Bebometro(),
         ),
-      ),
-    ),
+      );
+    },
   );
 }
